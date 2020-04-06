1cea742a8bd408de93601ae49ecf1ccca88fd5ef
Fernando Gomez <fagomezch@gmail.com>
2020-04-06 17:41:10 -0500

    Initial commit

---
+++ src/components/SliderCopy.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col slider__copy">
+    <span>Welcome to my World!</span>
+    <SliderTitle />
+    <h2 class="slider__subtitle">based in COL </h2>
+    <SocialNetworks />
+  </div>
+</template>
+
+<script>
+import SocialNetworks from './SocialNetworks.vue';
+import SliderTitle from './SliderTitle.vue';
+
+export default {
+  name: 'SliderCopy',
+  components: {
+    SliderTitle,
+    SocialNetworks,
+  },
+};
+</script>
+
+<style lang="scss">
+  @import '../styles/components/SliderCopy.scss';
+</style>
---
+++ src/components/SocialNetworks.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="social__networks">
+    <li>
+      <a href="https://www.linkedin.com/in/fagomezch/" target="_blank">
+        <span class="icon icon-linkedin"></span>
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/fagomezch" target="_blank">
+        <span class="icon icon-github"></span>
+      </a>
+    </li>
+    <li>
+      <a href="tel:573127838602">
+        <span class="icon icon-whatsapp"></span>
+      </a>
+    </li>
+    <li>
+      <a href="mailto:fagomezch@gmail.com">
+        <span class="icon icon-gmail"></span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SocialNetworks',
+};
+</script>
+
+<style lang="scss">
+   @import '../styles/components/SocialNetworks.scss';
+</style>
