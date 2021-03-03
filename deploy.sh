448e458b036aad2bff18e45d10cc6f44c9ffca88
Fernando Gomez <fernando.gomez@keyrus.com>
2021-03-02 23:21:47 -0500

    update deply settinga

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fagomezch/fagomezch.github.io master:gh-pages
+git push -f https://github.com/fagomezch/fagomezch.github.io master:gh-pages
 
 cd -
